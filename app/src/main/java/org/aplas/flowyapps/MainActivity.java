237309c5f47d48e350e1efa70e71ab76c36f4257
= <ramadhanjenius1@gmail.com>
2021-12-06 20:05:40 +0700

    Initial commit

---
+++ app/src/main/java/org/aplas/flowyapps/MainActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.flowyapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
